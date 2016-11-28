791e9f45b11392e34957c10b869638c62513905a
Hameedah Akande <hameedahakande@gmail.com>
2016-11-28 15:47:31 +0100

    Implement datatype function

---
+++ app/dataTypes.js
@@ -0,0 +1,38 @@
+function dataTypes(arg) {
+	if (typeof arg === "string") {
+		return arg.length
+	}
+
+	if (arg === null || arg === undefined) {
+		return "no value"
+	}
+
+	if (typeof arg === "boolean") {
+		return arg
+	}
+
+	if (typeof arg === "number") {
+		if (arg < 100) {
+			return "less than 100"
+		}
+		else if (arg > 100) {
+			return "more than 100"
+		}
+		else {
+			return  "equal to 100"
+		}
+	}
+
+	if (typeof arg === "object") {
+		if (arg.length > 2) {
+			return arg[2]
+		}
+		return undefined
+	}
+
+	if (typeof arg === "function") {
+		return arg(true)
+	}
+}
+
+module.exports = dataTypes
